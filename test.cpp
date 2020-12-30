011fc08a6baa934dd29784cc4357ca8f1e35a593
Y-Bohan <yuebohanc@163.com>
2020-12-30 16:08:59 +0800

    二叉树模板测试文件

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "BinTree.h"
+
+using namespace std;
+
+int main()
+{
+	BinTree<char> tree;			//´´½¨Ò»¿Å¿ÕÊ÷
+	BinNode<char> node[11];
+	char temp = 'A';
+	for (int i = 0; i < 10; i++)
+	{
+		node[i].InitData(temp);
+		temp++;
+	}
+	tree.insertRoot(node[0]);
+	tree.insertChild(0, node[1], 'L');
+	tree.insertChild(0, node[2], 'R');
+	tree.insertChild(1, node[3], 'L');
+	tree.insertChild(2, node[4], 'L');
+	tree.insertChild(2, node[5], 'R');
+	tree.insertChild(3, node[6], 'L');
+	tree.insertChild(3, node[7], 'R');
+	tree.insertChild(5, node[8], 'R');
+	tree.preOrderTree();
+	tree.deleteTreeNode(2);
+	tree.preOrderTree();
+	tree.inOrderTree();
+	tree.postOrderTree();
+	return 0;
+}
